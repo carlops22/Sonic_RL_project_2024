376e612f41c1958fb3de4b57a33bb31838196715
Carlos Venegas <cvenegasa@udec.cl>
2024-11-18 02:54:31 -0300

    oops

--- sonic-sb3-dqn.py
+++ sonic-sb3-dqn.py
@@ -8,7 +8,6 @@ from stable_baselines3.common.atari_wrappers import ClipRewardEnv, WarpFrame
 from stable_baselines3.common.vec_env import SubprocVecEnv, VecFrameStack, VecTransposeImage
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
-from stable_baselines3.common.buffers import PrioritizedReplayBuffer
 import retro
 import os
 
