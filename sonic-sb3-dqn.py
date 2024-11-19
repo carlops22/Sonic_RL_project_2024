b72b8419856125dd1f0ee2137947b21d8b9ccba1
Carlos Venegas <cvenegasa@udec.cl>
2024-11-18 23:11:06 -0300

    asdasfsd

--- sonic-sb3-dqn.py
+++ sonic-sb3-dqn.py
@@ -462,7 +462,7 @@ def main():
     ''''''
     # Create evaluation environment
     eval_env = make_env(args.game, curriculum_manager.get_current_level(), 0)()
-    eval_env = VecFrameStack(VecTransposeImage(SubprocVecEnv([lambda: eval_env])), n_stack=4)
+    eval_env = VecFrameStack(VecTransposeImage(SubprocVecEnv([make_env(args.game, curriculum_manager.get_current_level(), 0)])), n_stack=4)
     # Set up callbacks
     checkpoint_callback = CheckpointCallback(
         save_freq=50000 // args.num_envs,
